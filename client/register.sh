0a3e693bef9416527c139c1efb34fa23541a5e64
DatanoiseTV <gh@ext.no-route.org>
2018-02-22 16:32:00 +0100

    Clean up the client a bit.

--- client/register.sh
+++ client/register.sh
@@ -11,14 +11,13 @@ ROGRAM="${0##*/}"
 ARGS=( "$@" )
 SELF="$(readlink -f "${BASH_SOURCE[0]}")"
 
+dist=`grep DISTRIB_ID /etc/*-release | awk -F '=' '{print $2}'`
 if [ "$dist" != "Ubuntu" ]; then
   echo "Only Ubuntu is currently supported, sorry."
   exit 1
 fi
 
 [[ $UID == 0 ]] || exec sudo -p "[?] $PROGRAM must be run as root. Please enter the password for %u to continue: " "$SELF" "${ARGS[@]}"
-
-dist=`grep DISTRIB_ID /etc/*-release | awk -F '=' '{print $2}'`
 
 dec2ip () {
     local ip dec=$@
