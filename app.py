a26f62a69077af6c8311321e13ffe4d2b19a0137
DatanoiseTV <gh@ext.no-route.org>
2018-03-06 12:20:18 +0100

    Add celery worker queue.

--- app.py
+++ app.py
@@ -28,6 +28,28 @@ app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' +\
                                          os.path.join(basedir, 'peers.sqlite')
 
+from celery import Celery
+
+app.config.update(
+    CELERY_BROKER_URL='redis://localhost:6379',
+    CELERY_RESULT_BACKEND='redis://localhost:6379'
+)
+
+def make_celery(app):
+    celery = Celery(app.import_name, backend=app.config['CELERY_RESULT_BACKEND'],
+                    broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
+celery = make_celery(app)
+
 ###### USER SETTINGS ######
 app.config['API_SERVER_URL'] = 'https://netvm.inetgrid.net'
 app.config['WIREGUARD_SERVER_IP'] = '159.89.111.118'
